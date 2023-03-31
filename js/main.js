644a09904bab63f17d534f956bd625c1f4c9eac3
Will Armstrong Sr <107271171+warmstrongsr@users.noreply.github.com>
2023-03-31 18:34:42 -0500

    added starter css and screenshot of idea for presentation

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ async function getcityInfo(city){
         let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
         let data = await response.json();
         return data;
+        console.log(response);
     } catch(err){
         console.error(err);
     };
@@ -32,55 +33,60 @@ async function getcityInfo(city){
 
 // Function that will take in a city info object and create a card 
 function buildcityCard(cityObj){
-    
-    // Build a card with city info
-    let card = document.createElement('div');
-    card.className = 'card h-100';
+	// Build a card with city info
+	let card = document.createElement("div");
+	card.className = "card h-100";
 
-    // Create card body
-    let cardBody = document.createElement('div');
-    cardBody.className = 'card-body';
+	// Create card body
+	let cardBody = document.createElement("div");
+	cardBody.className = "card-body";
 
-    // Create city name/location elements
-    let cityTitle = document.createElement('h5');
-    cityTitle.innerHTML = `${cityObj.location.name}, ${cityObj.location.region}, ${cityObj.location.country}`;
-    cityTitle.className = 'card-title';
+	// Create city name/location elements
+	let cityTitle = document.createElement("h5");
+	cityTitle.innerHTML = `${cityObj.location.name}, ${cityObj.location.region}, ${cityObj.location.country}`;
+	cityTitle.className = "card-title";
 
-    // Create temp elements
-    let cityTemp = document.createElement('p');
-    cityTemp.innerHTML = `Tempature: ${cityObj.current.temp_c}C (${cityObj.current.temp_f}F)`;
-    cityTemp.className = 'card-temp';
+	// Create temp elements
+	let cityTemp = document.createElement("p");
+	cityTemp.innerHTML = `Tempature: ${cityObj.current.temp_c}°C | (${cityObj.current.temp_f}°F) / Feels Like: ${cityObj.current.feelslike_c}°C | (${cityObj.current.feelslike_f}°F)`;
+	cityTemp.className = "card-temp";
 
-    // Condition element (*my original first)
-    let cityCondition = document.createElement("p");
+	// Condition element (*my original first)
+	let cityCondition = document.createElement("p");
 	cityCondition.innerHTML = `Condition: ${cityObj.current.condition.text}`;
+	let conditionText = cityObj.current.condition.text;
+	let conditionIconUrl = "http:" + cityObj.current.condition.icon; // added http: to gget rif of random error message
+	let conditionIcon = document.createElement("img");
+	conditionIcon.src = conditionIconUrl;
+	conditionIcon.alt = conditionText;
+	cityCondition.append(conditionIcon);
+	
 	cityCondition.className = "card-text";
 
-    // Wind?  might as well since I baselined Chicago
-    let cityWind = document.createElement("p");
-    cityWind.innerHTML = `Wind: ${cityObj.current.wind_mph} mph (${cityObj.current.wind_kph} km/h) (${cityObj.current.wind_dir})`;
+	// Wind?  might as well since I baselined Chicago
+	let cityWind = document.createElement("p");
+	cityWind.innerHTML = `Wind: ${cityObj.current.wind_mph} mph (${cityObj.current.wind_kph} km/h) (${cityObj.current.wind_dir})`;
 	cityWind.className = "card-text";
-    
-    // Add elements to the card's body
-    cardBody.append(cityTitle);
+
+	// Add elements to the card's body
+	cardBody.append(cityTitle);
 	cardBody.append(cityTemp);
 	cardBody.append(cityCondition);
 	cardBody.append(cityWind);
-    
 
-    // Add card to card body
-    card.append(cardBody);
+	// Add card to card body
+	card.append(cardBody);
 
-    // Create a column for the card
-    let col = document.createElement('div');
-    col.className = 'col-12 col-md-6 col-lg-3 my-3';
+	// Create a column for the card
+	let col = document.createElement("div");
+	col.className = "col-12 col-md-6 col-lg-3 my-3";
 
-    // Add the card to the column
-    col.append(card);
+	// Add the card to the column
+	col.append(card);
 
-    // Get the city display row and add the column
-    let cityDisplay = document.getElementById('weatherDisplay');
-    weatherDisplay.prepend(col);
+	// Get the city display row and add the column
+	let cityDisplay = document.getElementById("weatherDisplay");
+	weatherDisplay.prepend(col);
 
-    console.log(cityObj);
+	console.log(cityObj);
 }
